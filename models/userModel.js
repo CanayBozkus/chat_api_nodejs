046529424c7f72adbd5b4fe67501caaf4ad0baf4
canaybozkus <bozkuscanay@gmail.com>
2023-09-20 20:49:33 +0300

    Added isSupportAgent field

--- models/userModel.js
+++ models/userModel.js
@@ -14,6 +14,10 @@ const UserSchema = new Schema({
     password: {
         required: true,
         type: String
+    },
+    isSupportAgent: {
+        type: Boolean,
+        default: false
     }
 }, { timestamps: { createdAt: 'createdAt' } })
 
