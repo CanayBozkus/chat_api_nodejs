8d42192aee14238b624da22a384408f256212e16
canaybozkus <bozkuscanay@gmail.com>
2023-09-21 22:19:12 +0300

    Added docker

--- redis.js
+++ redis.js
@@ -4,10 +4,24 @@ let client
 
 module.exports = {
     init: async () => {
-        client = await redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST)
-        await client.connect()
-        client.on('connection', () => console.log('Redis Connected'))
-        client.on('error', (err) => console.log("redis error", err))
+        try {
+            client = await redis.createClient({
+                host: process.env.REDIS_HOST,
+                port: process.env.REDIS_PORT,
+                socket: {
+                    host: process.env.REDIS_HOST,
+                    port: process.env.REDIS_PORT
+                },
+                legacyMode: true
+            })
+            await client.connect()
+            client.on('connection', () => console.log('Redis Connected'))
+            client.on('error', (err) => console.log("redis error", err))
+        }
+
+        catch (e){
+            console.log("Redis connection error", e, process.env.REDIS_HOST)
+        }
     },
     getClient: () => {
         return client
