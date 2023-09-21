696170cbbb7f48f7a3c5a8580e3cd412f19a082a
canaybozkus <bozkuscanay@gmail.com>
2023-09-21 10:34:05 +0300

    Added messageModel and message related js files

---
+++ models/messageModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const MessageSchema = new Schema({
+    from: {
+        type: Schema.ObjectId,
+        required: true
+    },
+    to: {
+        type: Schema.ObjectId,
+        required: true
+    },
+    message: {
+        type: Schema.ObjectId,
+        required: true
+    }
+}, { timestamps: { createdAt: 'createdAt' } })
+
+module.exports = mongoose.model('Message', MessageSchema)
